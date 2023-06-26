11367e47325fdd699d676c6c45f58fe2d3eaf74a
DmitriyKozlakov <118075568+dmitriykozlakov@users.noreply.github.com>
2023-06-27 00:08:31 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,18 @@
+﻿#include <QApplication>
+#include <QPushButton>
+#include <QMessageBox>
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+
+    QPushButton button("Click me!");
+
+    QObject::connect(&button, &QPushButton::clicked, [&]() {
+        QMessageBox::information(nullptr, "Clicked!", "You clicked the button!");
+        });
+
+    button.show();
+
+    return app.exec();
+}
